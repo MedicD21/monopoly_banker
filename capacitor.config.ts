db910f6770c52fe0b8bbeb966275d109c8421dc1
MedicD21 <DSchaaf12@me.com>
2025-12-13 10:05:55 -0500

    appid change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.dushin.monopolybanker.app",
+  appId: "com.dushin.digitalbanker.app",
   appName: "Digital Banker",
   webDir: "dist",
 };
