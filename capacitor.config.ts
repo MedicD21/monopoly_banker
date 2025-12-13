4b97181434aa6fa7517db2861a230443c005b69d
MedicD21 <DSchaaf12@me.com>
2025-12-13 08:51:41 -0500

    android and ios add

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "Monopoly Banker",
-  appName: "com.dushin.monopolybanker",
+  appId: "com.dushin.monopolybanker.app",
+  appName: "Monopoly Banker",
   webDir: "dist",
 };
 
