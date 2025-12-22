d0ae0338a537f6a65b9a173f0e03ac2333d6f60a
MedicD21 <DSchaaf12@me.com>
2025-12-22 15:04:17 -0500

    fixing up github

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.dushin.digitalbanker.app",
-  appName: "Digital Banker",
-  webDir: "dist",
+  appId: 'com.dushin.digitalbanker.app',
+  appName: 'digital banker',
+  webDir: 'dist'
 };
 
 export default config;
