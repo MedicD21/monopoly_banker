9f1f492a162597f8eb79cc89bfdd75f156e610e4
MedicD21 <DSchaaf12@me.com>
2025-12-25 17:18:09 -0500

    white fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dushin.digitalbanker.app',
   appName: 'digital banker',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    cleartext: true,
+    allowNavigation: ['*']
+  },
+  ios: {
+    contentInset: 'automatic'
+  }
 };
 
 export default config;
